16403991f71b27ee98c8048fda2cbf70e5364236
Jian Chang <45157140+jchangz@users.noreply.github.com>
2024-11-17 23:31:52 -0700

    refactor: add init

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,30 @@
 import * as THREE from "three"
 import "./style.css"
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+let camera: THREE.PerspectiveCamera
+let scene: THREE.Scene
+let renderer: THREE.WebGLRenderer
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.querySelector<HTMLDivElement>("#app")!.appendChild(renderer.domElement)
+let cube: THREE.Mesh
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+init()
 
-camera.position.z = 5
+function init() {
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera.position.z = 5
+
+  renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setAnimationLoop(animate)
+  document.querySelector<HTMLDivElement>("#app")!.appendChild(renderer.domElement)
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  cube = new THREE.Mesh(geometry, material)
+
+  scene = new THREE.Scene()
+  scene.add(cube)
+}
 
 function animate() {
   cube.rotation.x += 0.01
@@ -21,4 +32,3 @@ function animate() {
 
   renderer.render(scene, camera)
 }
-renderer.setAnimationLoop(animate)
