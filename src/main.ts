b68449343572f291310866f8f9f793173f7951e6
Jian Chang <45157140+jchangz@users.noreply.github.com>
2024-11-17 23:35:22 -0700

    feat: add window resize, orbit controls

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import * as THREE from "three"
+import { OrbitControls } from "three/addons/controls/OrbitControls.js"
 import "./style.css"
 
 let camera: THREE.PerspectiveCamera
 let scene: THREE.Scene
 let renderer: THREE.WebGLRenderer
+let controls: OrbitControls
 
 let cube: THREE.Mesh
 
@@ -18,12 +20,23 @@ function init() {
   renderer.setAnimationLoop(animate)
   document.querySelector<HTMLDivElement>("#app")!.appendChild(renderer.domElement)
 
+  controls = new OrbitControls(camera, renderer.domElement)
+
   const geometry = new THREE.BoxGeometry(1, 1, 1)
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
   cube = new THREE.Mesh(geometry, material)
 
   scene = new THREE.Scene()
   scene.add(cube)
+
+  window.addEventListener("resize", onWindowResize)
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
 function animate() {
