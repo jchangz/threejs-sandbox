2e9b8416f6d4c9ae36f29ec0df8227d6958c0ed9
Jian Chang <45157140+jchangz@users.noreply.github.com>
2026-02-03 00:11:04 -0700

    fix: gltf-validator types

--- src/types/gltf-validator.d.ts
+++ src/types/gltf-validator.d.ts
@@ -1,4 +1,61 @@
 declare module "gltf-validator" {
+  const enum Severity {
+    Error = 0,
+    Warning = 1,
+    Information = 2,
+    Hint = 3,
+  }
+
+  interface Messages {
+    code: string
+    message: string
+    severity: Severity
+    pointer: string
+  }
+
+  interface Issues {
+    numErrors: number
+    numWarnings: number
+    numInfos: number
+    numHints: number
+    messages: Messages[]
+    truncated: boolean
+  }
+
+  interface Resources {
+    pointer: string
+    mimeType: string
+    storage: string
+    uri: string
+  }
+
+  interface Info {
+    version: string
+    generator: string
+    resources: Resources[]
+    animationCount: number
+    materialCount: number
+    hasMorphTargets: boolean
+    hasSkins: boolean
+    hasTextures: boolean
+    hasDefaultScene: boolean
+    drawCallCount: number
+    totalVertexCount: number
+    totalTriangleCount: number
+    maxUVs: number
+    maxInfluences: number
+    maxAttributes: number
+  }
+
+  interface Result {
+    uri: string
+    mimeType: string
+    validatorVersion: string
+    validatedAt: string
+    issues: Issues
+    info: Info
+  }
+
   /**
    * Validates a GLTF/GLB asset provided as a Uint8Array.
    * @param data - The glTF/GLB file content as bytes.
@@ -11,7 +68,5 @@ declare module "gltf-validator" {
       maxSeverity?: number
       uri?: string
     },
-  ): Promise<any> // Replace 'any' with specific report interface if needed
-
-  // Add other exports if necessary
+  ): Promise<Result>
 }
